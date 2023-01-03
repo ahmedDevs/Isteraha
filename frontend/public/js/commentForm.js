b9c6542d0708dd4ea1222df0914acc022361dabd
ahmedDevs <ahmed.soft.dev@gmail.com>
2023-01-02 19:23:32 -0500

    i

--- frontend/public/js/commentForm.js
+++
@@ -1,6 +0,0 @@
-const commentForm = document.getElementById('commentForm').submit()
-commentForm.addEventListener('onKeypress', e => {
-    if(e.keyCode == 13) {
-        e.preventDefault()
-    }
-})
