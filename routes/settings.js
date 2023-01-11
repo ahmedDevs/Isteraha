c8ca90dee0d1954bd58109a52129e249d67952cb
ahmedDevs <ahmed.soft.dev@gmail.com>
2023-01-11 09:01:31 -0500

    Fix bug when updating profile image and profile information

--- routes/settings.js
+++ routes/settings.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../middleware/multer");
 const networksController = require("../controllers/networks")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
@@ -7,6 +8,6 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 router.get("/network/:id", ensureAuth, networksController.getNetworkSettings)
 // router.put("/user", ensureAuth, networksController.putUserSettings)
 router.post("/network/:id/save", ensureAuth, networksController.postNetworkSettings)
-router.put("/user/save", ensureAuth, networksController.putUserSettings)
+router.put("/user/save", upload.single("file"), ensureAuth, networksController.putUserSettings)
 
 module.exports = router;
